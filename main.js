c1dba02f7b62e19771c0f939df8a70ef4f952c12
Ariel March <arielmarch67@gmail.com>
2022-12-30 17:44:27 -0300

    third commit

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 
-
-const formulario = document.querySelector("#formulario");
+//clases
 
 class Cañas {
     constructor(marca, nombre, medida, tramos, precio, stock){
@@ -12,7 +11,11 @@ class Cañas {
         this.stock = stock;
     }
 }
-let divCañas = document.querySelector("#cañas");
+
+// variables 
+const boton = document.getElementById("btn");
+const divCañas = document.getElementById("cañas");
+
 const listaCañas = JSON.parse(localStorage.getItem("cañas")) || [
 {id:1, marca:"ALBATROS", nombre:"DORADILLO", medida:"180", tramos: 1, precio: 7500, stock: 4},
 {id:2, marca:"ALBATROS", nombre: "HULK", medida:"210", tramos: 2, precio: 5000, stock: 3},
@@ -22,41 +25,46 @@ const listaCañas = JSON.parse(localStorage.getItem("cañas")) || [
 {id:6, marca:"WATERDOG", nombre:"COLVILLE", medida:"390", tramos: 2, precio: 13000, stock: 1},
 ];
 
-const nuevaCaña = () => {
-    let marca = document.getElementById("marca").value;
-    let nombre = document.getElementById("nombre").value;
-    let medida = document.getElementById("medida").value;
-    let tramos = document.getElementById("tramos").value;
-    let precio = document.getElementById("precio").value;
-    let stock = document.getElementById("stock").value;
 
-    let cañaNueva = new Cañas(marca, nombre, medida, tramos, precio, stock);
 
+
+// funciones 
+
+const crearCaña = () => {
+    const marca = document.getElementById("marca").value;
+    const nombre = document.getElementById("nombre").value;
+    const medida = document.getElementById("medida").value;
+    const tramos = document.getElementById("tramos").value;
+    const precio = document.getElementById("precio").value;
+    const stock = document.getElementById("stock").value;
+
+    const cañaNueva = new Cañas(marca, nombre, medida, tramos, precio, stock);
     listaCañas.push (cañaNueva);
     localStorage.setItem("cañas", JSON.stringify(listaCañas));
-
-    return listaCañas;
+    // return listaCañas;
 }
 
-formulario.onsubmit = (e) => {
-    e.preventDefault();
-    nuevaCaña();
-    mostrarCañas()
-}
-const mostrarCañas = ()=> {
-    const cañasGuardadas =  JSON.parse(localStorage.getItem("cañas"));
-
-    cañasGuardadas.forEach(caña => {
+const pintarCañas = ()=> {
+    listaCañas.forEach(caña => {
         divCañas.innerHTML += `
-                                <div>
-                                    <h3> Marca: ${caña.marca}</h3>
-                                    <h3> Nombre : ${caña.nombre}</h3>
-                                    <p> Medida: ${caña.medida}</p>
-                                    <p> Tramos: ${caña.tramos}</p>
-                                    <p> Precio: $${caña.precio}</p>
-                                    <p> Stock: ${caña.stock}</p>
-                                </div>
-                                `
+        <div>
+        <h3> Marca: ${caña.marca}</h3>
+        <h3> Nombre : ${caña.nombre}</h3>
+        <p> Medida: ${caña.medida}</p>
+        <p> Tramos: ${caña.tramos}</p>
+        <p> Precio: $${caña.precio}</p>
+        <p> Stock: ${caña.stock}</p>
+        </div>
+        `
     })
-
+    
 }
+
+//eventos 
+
+boton.addEventListener("click", (e)=> {
+    // e.preventDefault();
+    crearCaña();
+})
+pintarCañas();
+
